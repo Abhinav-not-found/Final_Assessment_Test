20b8b182860f2287bfc8f188faef88b86eb36159
Abhinav-not-found <abhinavisgreat18@gmail.com>
2023-07-18 17:54:46 +0530

    modified the files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import PersonSection from './components/Personsection'
 
+
 export default function App() {
   return (
     <div>
       <PersonSection></PersonSection>
+
     </div>
   )
 }
